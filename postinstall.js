7a18b81a4c5a8f7885584196bd0932fe62a02b55
Ayman Zaakouki Boukraa <thettayman@gmail.com>
2021-06-16 20:20:20 +0200

    added postinstall message
    To do: check if console supports color, if not, remove colors

---
+++ postinstall.js
@@ -0,0 +1,11 @@
+var banner =
+  "\u001B[96mThank you for using cronos-xp (\u001B[94m https://github.com/cronos-team/cronos-xp \u001B[96m)[0m\n\n" +
+  "\u001B[96mFor more packages check our GitHub profile.\nIf you have any issue feel free to join the Discord support server \u001B[0m\n" +
+  "\u001B[96m> Discord\u001B[94m https://discord.gg/eXrQv9e699 \u001B[0m\n" +
+  "\u001B[96m> Github\u001B[94m https://github.com/cronos-team \u001B[0m\n";
+
+  function showBanner() {
+    console.log(banner);
+  }
+
+  showBanner()
