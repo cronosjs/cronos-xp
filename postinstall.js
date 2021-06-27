74b7ea75f0d0e803da58f7126a01eba89d9fabc7
Ayman Zaakouki Boukraa <thettayman@gmail.com>
2021-06-27 13:59:06 +0200

    updated github links

--- postinstall.js
+++ postinstall.js
@@ -1,8 +1,8 @@
 var banner =
-  "\u001B[96mThank you for using cronos-xp (\u001B[94m https://github.com/cronos-team/cronos-xp \u001B[96m)\u001B[0m\n\n" +
+  "\u001B[96mThank you for using cronos-xp (\u001B[94m https://github.com/cronosjs/cronos-xp \u001B[96m)\u001B[0m\n\n" +
   "\u001B[96mFor more packages check our GitHub profile.\nIf you have any issue feel free to join the our Discord server \u001B[0m\n" +
   "\u001B[96m> Discord\u001B[94m https://discord.gg/eXrQv9e699 \u001B[0m\n" +
-  "\u001B[96m> Github\u001B[94m https://github.com/cronos-team \u001B[0m\n";
+  "\u001B[96m> Github\u001B[94m https://github.com/cronosjs \u001B[0m\n";
 
   function showBanner() {
     console.log(banner);
